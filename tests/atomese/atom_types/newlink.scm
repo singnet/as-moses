61bf8e0ae18ec3ecae2bb35c595b8e62153b9c1c
Bitseat <bitseatt@gmail.com>
2019-10-11 15:32:58 +0300

    Add test

--- tests/atomese/atom_types/newlink.scm
+++ tests/atomese/atom_types/newlink.scm
@@ -1,4 +1,8 @@
 (use-modules (opencog) (opencog exec) (opencog asmoses))
 
-(define test
+(define test_or
 (New (TypeNode "OrLink") (ListLink (Predicate "P") (Predicate "Q"))))
+
+(define test_and
+(New (TypeNode "AndLink") (ListLink (Predicate "P") (Predicate "Q"))))
+
